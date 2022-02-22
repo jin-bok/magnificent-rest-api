c5f658c4d5c55fcc5c24a6426e4694f80c7ba9ef
Jin Bok <jin@quatredeux.com>
2022-02-22 20:49:57 -0300

    test: ensure LoginRouter returns HTTP code 500 if AuthUseCase is invalid

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -80,4 +80,28 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
+
+  test('should return HTTP code 500 if AuthUseCaseSpy isn\'t injected', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@any_provider.any_domain',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('should return HTTP code 500 if AuthUseCaseSpy has no auth method', () => {
+    const sut = new LoginRouter({})
+    const httpRequest = {
+      body: {
+        email: 'any_email@any_provider.any_domain',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
 })

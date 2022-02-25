1dea8e04e2b0b1cd0ceae51eba3777233cf887bd
Jin Bok <jin@quatredeux.com>
2022-02-25 07:43:26 -0300

    test: ensure EmailValidator returns false if validator library returns false

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,13 +1,22 @@
+const validator = require('validator')
+
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
 describe('Email Validator', () => {
-  test('should return true is validator library returns true', async () => {
+  test('should return true if validator library returns true', async () => {
     const sut = new EmailValidator()
     const isEmailValid = sut.isValid('valid_email@valid_provider.valid_domain')
     expect(isEmailValid).toBe(true)
+  })
+
+  test('should return false if validator library returns false', async () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email@invalid_provider.invalid_domain')
+    expect(isEmailValid).toBe(false)
   })
 })

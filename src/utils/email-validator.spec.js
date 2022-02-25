02aec737b2180887b2fa7d17b4b0af8a44a5404d
Jin Bok <jin@quatredeux.com>
2022-02-25 07:46:44 -0300

    refactor(tests): implement makeSut to EmailValidator

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,16 +6,20 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('should return true if validator library returns true', async () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@valid_provider.valid_domain')
     expect(isEmailValid).toBe(true)
   })
 
   test('should return false if validator library returns false', async () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@invalid_provider.invalid_domain')
     expect(isEmailValid).toBe(false)
   })
